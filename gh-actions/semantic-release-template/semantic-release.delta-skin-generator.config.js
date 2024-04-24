8906eb7fdb30fcd9c19aa715720a32072aff1b4a
Frank Steiler <frank@steilerdev.de>
2024-04-24 18:55:33 +0200

    fix: Removing default branch definition

--- gh-actions/semantic-release-template/semantic-release.delta-skin-generator.config.js
+++ gh-actions/semantic-release-template/semantic-release.delta-skin-generator.config.js
@@ -2,7 +2,6 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-    branches: ["main"],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
